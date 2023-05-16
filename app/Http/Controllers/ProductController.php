4246adb7e1cfaed0d6ba7ba19515d55b1c553965
sandeepptpss <sandeepptpss@gmail.com>
2023-05-16 22:24:46 +0530

    New update add product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,27 +1,27 @@
 <?php
-
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 
 class ProductController extends Controller
 {
-    function addProduct(Request $req)
+    public function addProduct(Request $req)
     {
-        $product
-         = new Product;
+        $imageNames = '';
+        $product = new Product;
         $product->title = $req->input("title");
         $product->price = $req->input("price");
         $product->description = $req->input("description");
-
+        
         if ($image = $req->file('image')) {
-            $imageDestinationPath = 'uploads/';
+            $imageDestinationPath = 'upload/';
             $productImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
             $image->move($imageDestinationPath, $productImage);
             $product->image = $productImage;
         }
-
+        
         $product->save();
+        
         return $req->input();
     }
 }
