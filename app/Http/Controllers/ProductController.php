479af42de3f85a62830c3dcfa104459bcfa553d0
sandeepptpss <sandeepptpss@gmail.com>
2023-05-18 23:24:08 +0530

    product Controller few change

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,22 +8,36 @@ class ProductController extends Controller
     public function addProduct(Request $req)
     {
         $imageNames = '';
+
         $product = new Product;
+
+        
         $product->title = $req->input("title");
         $product->price = $req->input("price");
         $product->description = $req->input("description");
-        
-        if ($image = $req->file('image')) {
+        if ($image = $req->file('image')){
             $imageDestinationPath = 'upload/';
             $productImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
             $image->move($imageDestinationPath, $productImage);
             $product->image = $productImage;
         }
-        
         $product->save();
-        
         return $req->input();
     }
+
+public function Display(Request $req){
+         $product= Product::all();
+         return $product;
+}
+public function viewDetail($id){
+    $product = Product::find($id);
+    return $product;
+  
+}
+
+   
+
+
 }
 
 
